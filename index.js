f5cd3a7058079077ee900415f274209e490e2160
Tenakskd <anzuday7777@gmail.com>
2024-11-22 10:34:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,13 +34,6 @@ app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
 template = Jinja2Templates(directory='templates').TemplateResponse
-@app.get("/", response_class=HTMLResponse)
-def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
-    if check_cokie(yuki):
-        response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
-        return template("home.html",{"request": request})
-    print(check_cokie(yuki))
-    return redirect("/word")
 //レギュラー
 app.get('/w/:id', async (req, res) => {
   const videoId = req.params.id;
